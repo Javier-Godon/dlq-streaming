b493b3ef157e179aebcbbb6bee006918112ddc02
Javier Godon <javier.godon@gmail.com>
2026-05-23 19:10:57 +0200

    project initialization

---
+++ src/main/java/es/bluesolution/dlq_streaming/DlqStreamingApplication.java
@@ -0,0 +1,13 @@
+package es.bluesolution.dlq_streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlqStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlqStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/bluesolution/dlq_streaming/DlqStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package es.bluesolution.dlq_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlqStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
